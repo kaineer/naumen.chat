941d21f5ad9dfffbe7bcd3bcb864f56209e8f902
Tangerine Cat <kaineer@gmail.com>
2012-05-22 15:12:24 +0600

    Removed duplication in javascript

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,15 +1,39 @@
+var prependData = function(data) {
+	if(data.collection != null) {
+	  var html = "";
+	  _(data.collection).each(function(msg) {
+	  	html += "<dt>" + msg.user_name + "</dd>";
+	  	html += "<dd>" + msg.text + "</dd>";
+	  });
+
+	  $("#list").prepend(html);
+	}
+};
+
 reloadFunc = function() {
 	$.getJSON('/messages.json', {}, function(data) {
-		if(data.collection != null) {
-		  var html = "";
-		  _(data.collection).each(function(msg) {
-		  	html += "<dt>" + msg.user_name + "</dd>";
-		  	html += "<dd>" + msg.text + "</dd>";
-		  });
-
-		  $("#list").prepend(html);
-		}
+		prependData(data);
 
    	window.setTimeout(reloadFunc, 4000);
 	});
 };
+
+$(function() {
+	$(".index-page form").submit(function() {
+		$.ajax({
+			type: "POST",
+			dataType: "JSON",
+			url: "/messages.json",
+			data: {
+				message: {
+					text: $("#message_text").val()
+				}
+			},
+			success: prependData
+		});
+
+		$("#message_text").val("");
+
+		return false;
+	});
+});
